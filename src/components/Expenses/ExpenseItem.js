37055243ba2f1c892e87f5065352ac19e91b1c7c
Kim-Jeongyoung <jeongyoung2@gmail.com>
2021-08-13 23:37:03 -0700

    adding eventHandler and useState Hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
@@ -6,18 +6,20 @@ import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
   // function clickHandler() {}
-  let title = props.title;
+  //useState() manage by React somewhere in memory
+  const [title, setTitle] = useState(props.title);
+  console.log('ExpenseItem evaluated by React');
 
   const clickHandler = () => {
-    title = 'Updated!';
-    console.log('Clicked!!!');
+    setTitle('Updated!'); //assign title with setTitle
+    console.log('title');
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       {/* listening to events */}
@@ -34,3 +36,14 @@ const ExpenseItem = (props) => {
 };
 
 export default ExpenseItem;
+
+// useEffect(
+//   () => console.log(title),
+//   [title]
+// );
+
+// 48. <h2>{props.title}</h2> => <h2>{title}</h2>State is updating but  you are not using the state variable anywhere so you cannot see the change.
+
+// title is state variable, props.title is the prop value title is updating, not the props.title.
+
+// use title instead of props.title in h2 and change will be reflected
