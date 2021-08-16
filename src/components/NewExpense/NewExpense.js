123ecc82ee6bda9513b8af5c0ee392256e92e042
Kim-Jeongyoung <jeongyoung2@gmail.com>
2021-08-16 16:06:50 -0700

    Adding NewExpense.js and ExpenseForm

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+
+const NewExpense = () => {
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
+    </div>
+  );
+};
+
+export default NewExpense;
+// import ExpenseForm from './ExpenseForm';
+// import './NewExpense.css';
+
+// const NewExpense = () => {
+//   return (
+//     <div calssName="new-expense">
+//       <ExpenseForm />
+//     </div>
+//   );
+// };
+
+// export default NewExpense;
